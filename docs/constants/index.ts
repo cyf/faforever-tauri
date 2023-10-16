18e35c45851c4ce28fd93af62af1656f753610e6
kjxbyz <kjxbyz888@outlook.com>
2023-10-16 23:14:29 +0800

    docs: new docs

---
+++ docs/constants/index.ts
@@ -0,0 +1,2 @@
+export const cacheLngKey: string = "__cyf_blog_lng__";
+export const cacheThemeKey: string = "__cyf_blog_theme__";
---
+++ docs/process.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    VERCEL_GIT_COMMIT_SHA: string;
+    NEXT_PUBLIC_GOOGLE_ID: string;
+  }
+}
