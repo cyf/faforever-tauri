63c4261373f32beecb3522c207c235639f6eae7a
kjxbyz <kjxbyz@163.com>
2023-10-22 16:40:16 +0800

    fix: cache disabled

--- docs/request/index.ts
+++ docs/request/index.ts
@@ -4,6 +4,8 @@ export async function latestRelease() {
   const resp = await fetch("/faforever/api/releases/latest/", {
     method: "GET",
     headers,
+    // https://nextjs.org/docs/app/building-your-application/caching
+    cache: "no-store",
   });
   return await resp.json();
 }
