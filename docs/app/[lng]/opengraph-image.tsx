af85ba0e148cf4a444af90750026d47a71fabba1
kjxbyz <kjxbyz@163.com>
2023-11-09 21:17:01 +0800

    fix: update twitter:image:alt

--- docs/app/[lng]/opengraph-image.tsx
+++ docs/app/[lng]/opengraph-image.tsx
@@ -2,7 +2,7 @@
 import { ImageResponse } from "next/og";
 
 export const runtime = "edge";
-export const alt = "Precedent - Building blocks for your Next.js project";
+export const alt = "FaForever";
 export const contentType = "image/png";
 
 export default async function OG() {
