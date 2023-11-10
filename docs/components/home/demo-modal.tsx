c895d7ac51255e42a36d15beb4521e9bb66512b4
kjxbyz <kjxbyz@163.com>
2023-11-10 20:47:01 +0800

    fix: replace all occurrences of `Precedent`

--- docs/components/home/demo-modal.tsx
+++ docs/components/home/demo-modal.tsx
@@ -20,20 +20,17 @@ const DemoModal = ({
     <Modal showModal={showDemoModal} setShowModal={setShowDemoModal}>
       <div className="w-full overflow-hidden md:max-w-md md:rounded-2xl md:border md:border-gray-100 md:shadow-xl">
         <div className="flex flex-col items-center justify-center space-y-3 bg-white px-4 py-6 pt-8 text-center md:px-16">
-          <a href="https://precedent.dev">
+          <a href="https://chenyifaer.com/faforever">
             <Image
               src={`${basePath}/logo.png`}
-              alt="Precedent Logo"
+              alt="FaForever Logo"
               className="h-10 w-10 rounded-full"
               width={20}
               height={20}
             />
           </a>
-          <h3 className="font-display text-2xl font-bold">Precedent</h3>
-          <p className="text-sm text-gray-500">
-            Precedent is an opinionated collection of components, hooks, and
-            utilities for your Next.js project.
-          </p>
+          <h3 className="font-display text-2xl font-bold">FaForever</h3>
+          <p className="text-sm text-gray-500">A music desktop client.</p>
         </div>
       </div>
     </Modal>
