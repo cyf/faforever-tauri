13ee03c4dbcc61876390b9d76061067b05d4e0a3
kjxbyz <kjxbyz@163.com>
2023-11-20 09:22:32 +0800

    fix: dynamic route

--- docs/app/api/releases/latest/route.ts
+++ docs/app/api/releases/latest/route.ts
@@ -5,8 +5,8 @@ const octokit = new Octokit({
 });
 
 // https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
-// export const dynamic = 'force-dynamic'
-export const fetchCache = "force-no-store";
+export const dynamic = "force-dynamic";
+// export const fetchCache = "force-no-store";
 
 export async function GET() {
   try {
