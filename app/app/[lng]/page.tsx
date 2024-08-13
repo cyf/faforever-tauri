4fa35d9ede4caf6bc3c4094aaf680ae267450f07
kjxbyz <kjxbyz888@outlook.com>
2024-08-14 04:01:36 +0800

    fix: build error

--- app/app/[lng]/page.tsx
+++ app/app/[lng]/page.tsx
@@ -13,7 +13,7 @@ import {
   SiInstagram,
   SiYoutube,
   SiTwitch,
-  SiTwitter,
+  SiX,
   SiTelegram,
 } from "react-icons/si";
 import { BsWindowDesktop } from "react-icons/bs";
@@ -216,13 +216,13 @@ const features = [
     url: "https://weibo.com/u/7357828611",
   },
   {
-    title: "Twitter",
+    title: "X",
     description:
       "Jumpstart your next project by deploying Precedent to [Vercel](https://vercel.com/) in one click.",
     demo: (
-      <SiTwitter className="h-24 w-24 text-gray-600 transition-all dark:text-white/80" />
+      <SiX className="h-24 w-24 text-gray-600 transition-all dark:text-white/80" />
     ),
-    url: "https://twitter.com/yifaer_chen",
+    url: "https://x.com/yifaer_chen",
   },
   {
     title: "Telegram",
